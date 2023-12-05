2e040bab728e02dcd094a34c05a617e2689dc090
Patryk Stępień <fighting.dreamer.0@gmail.com>
2023-12-05 22:37:18 +0100

    Add tests

---
+++ lib/iter/filter.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "vitest";
+import { filter } from "./filter";
+
+test.each([
+  { value: [], fn: (v) => v == 5, expected: [] },
+  { value: [1, 2, 4, 5], fn: (v) => v == 3, expected: [] },
+  {
+    value: [1, 2, 3, 4, 5],
+    fn: (v: any) => [1, 3, 5].includes(v),
+    expected: [1, 3, 5],
+  },
+])("filter($value, $divider) -> $expected", ({ value, fn, expected }) => {
+  const ivalue = value[Symbol.iterator]();
+  expect(Array.from(filter(ivalue, fn))).toEqual(expected);
+});
